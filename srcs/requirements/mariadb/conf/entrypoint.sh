7cb6574d96b38388c341103f99b029c6388bf063
Thibaud Maitre <thmaitre@z2r9p5.42lyon.fr>
2026-05-29 00:41:43 +0200

    wip wordpress: try to config but mariadb is not responding

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -2,20 +2,20 @@
 set -e
 
 get_secret() {
-  local secret_name=$1
-  local default_value=$2
-  if [ -f "/run/secrets/${secret_name}" ]; then
-    cat "/run/secrets/${secret_name}"
-  else
-    echo "$default_value"
-  fi
+	local secret_name=$1
+	local default_value=$2
+	if [ -f "/run/secrets/${secret_name}" ]; then
+		cat "/run/secrets/${secret_name}"
+	else
+		echo "$default_value"
+	fi
 }
 
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-  mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
 MYSQL_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD_FILE)
@@ -30,5 +30,7 @@ GRANT ALL PRIVILEGES ON ${MYSQL_WP_DATABASE}.* TO '${MYSQL_WP_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
+
+echo "bind-address=0.0.0.0" >>/etc/my.cnf.d/mariadb-server.cnf
 
 exec mariadbd --user=mysql --datadir=/var/lib/mysql --init-file=/var/lib/mysql/init.sql
