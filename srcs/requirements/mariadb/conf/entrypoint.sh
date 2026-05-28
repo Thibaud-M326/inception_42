24c0ae3aa028951799d835b6960a60429a1ff9fb
root <root@thmaitre--.42.fr>
2026-05-28 18:14:10 +0200

    mariadb container setup done

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-
 get_secret() {
   local secret_name=$1
   local default_value=$2
@@ -14,22 +11,22 @@ get_secret() {
   fi
 }
 
-MYSQL_PASSWORD=$(get_secret "mysql_wp_password.txt" "wp_fallback_pass")
-
-if [ -f "$MYSQL_WP_PASSWORD" ]; then
-  MYSQL_WP_PASSWORD=$(cat "$MYSQL_WP_PASSWORD")
-fi
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
+MYSQL_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD_FILE)
+MYSQL_WP_PASSWORD=$(cat $MYSQL_WP_PASSWORD_FILE)
+
 cat <<EOF >/var/lib/mysql/init.sql
-CREATE DATABASE IF NOT EXISTS wp_database;
+CREATE DATABASE IF NOT EXISTS ${MYSQL_WP_DATABASE};
 
-CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY 'motdepasse';
+CREATE USER IF NOT EXISTS '${MYSQL_WP_USER}'@'%' IDENTIFIED BY '${MYSQL_WP_PASSWORD}';
 
-GRANT ALL PRIVILEGES ON wp_database.* TO 'wp_user'@'%';
+GRANT ALL PRIVILEGES ON ${MYSQL_WP_DATABASE}.* TO '${MYSQL_WP_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
